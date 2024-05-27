a8cacc00cb7a1472dd91b5f82c2635623466cfdf
Garvit Jethwani <44478489+Garvit-Jethwani@users.noreply.github.com>
2024-05-27 12:01:45 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,18 @@
 package hello;
 
 public class HelloWorld {
+  
   public static void main(String[] args) {
     System.out.println("Hello Roost");
+    
+    // Example usage of the addition method
+    int a = 10;
+    int b = 5;
+    System.out.println("Addition: " + add(a, b));
+  }
+
+  // Method to add two integers
+  public static int add(int x, int y) {
+    return x + y;
   }
 }
